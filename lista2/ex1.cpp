077eb146886db5a10d72893beb3e4e131fd8e331
giovannirosario <giovanni.rosario@hotmail.com>
2019-03-19 23:03:31 -0300

    Finished ex1 lista2

--- lista2/ex1.cpp
+++ lista2/ex1.cpp
@@ -64,7 +64,52 @@ double newton(double p0, int &it_count) {
 
 }
 
+double secant(double p0, double p1, int &it_count) {
+    it_count = 2;
+    double q0 = f(p0);
+    double q1 = f(p1);
+
+    while (it_count < IT_MAX) {
+        double p = p1 - q1 * (p1 - p0)/(q1 - q0);
+        if (std::abs(p-p1) < 1/pow(10,PRECISION)) {
+            return p;
+        } else {
+            it_count++;
+            p0 = p1;
+            q0 = q1;
+            p1 = p;
+            q1 = f(p);        
+        }
+    }
+    return 0;
+}
+
+double regulaFalsi(double p0, double p1, int &it_count) {
+    it_count = 2;
+    double q0 = f(p0);
+    double q1 = f(p1);
+
+    while (it_count < IT_MAX) {
+        double p = p1 - q1 * (p1 - p0)/(q1 - q0);
+        if (std::abs(p-p1) < 1/pow(10,PRECISION)) {
+            return p;
+        } else {
+            it_count++;
+            double q = f(p);
+            if(q*q1 < 0) {
+                p0 = p1;
+                q0 = q1;
+            }
+            p1 = p;
+            q1 = q;
+        }
+    }
+}
+
 int main () {
+
+    std::cout << "BISECTION \n" << std::endl;
+
     int it_count = 0;
     std::cout << std::fixed << std::setprecision(PRECISION) << bisection(-4,-1.57,it_count);
     std::cout << " took " << it_count << " iterations" << std::endl;
@@ -77,15 +122,45 @@ int main () {
     std::cout << std::fixed << std::setprecision(PRECISION) << bisection(2.7,5,it_count);
     std::cout << " took " << it_count << " iterations" << std::endl;
 
+    std::cout << "\n NEWTON \n" << std::endl;
+
     it_count = 0;
     std::cout << std::fixed << std::setprecision(PRECISION) << newton(-1.57,it_count);
     std::cout << " took " << it_count << " iterations" << std::endl;
 
-     it_count = 0;
+    it_count = 0;
     std::cout << std::fixed << std::setprecision(PRECISION) << newton(2.7,it_count);
     std::cout << " took " << it_count << " iterations" << std::endl;
 
-     it_count = 0;
+    it_count = 0;
     std::cout << std::fixed << std::setprecision(PRECISION) << newton(-1.2,it_count);
+    std::cout << " took " << it_count << " iterations" << std::endl;
+
+    std::cout << "\n SECANT \n" << std::endl;
+
+    it_count = 0;
+    std::cout << std::fixed << std::setprecision(PRECISION) << secant(-5,-1.57,it_count);
+    std::cout << " took " << it_count << " iterations" << std::endl;
+
+    it_count = 0;
+    std::cout << std::fixed << std::setprecision(PRECISION) << secant(-1.5,0,it_count);
+    std::cout << " took " << it_count << " iterations" << std::endl;
+
+    it_count = 0;
+    std::cout << std::fixed << std::setprecision(PRECISION) << secant(2,5,it_count);
+    std::cout << " took " << it_count << " iterations" << std::endl;
+
+    std::cout << "\n REGULA FALSI \n" << std::endl;
+
+    it_count = 0;
+    std::cout << std::fixed << std::setprecision(PRECISION) << regulaFalsi(-4,-1.57,it_count);
+    std::cout << " took " << it_count << " iterations" << std::endl;
+
+    it_count = 0;
+    std::cout << std::fixed << std::setprecision(PRECISION) << regulaFalsi(-1.57,2.7,it_count);
+    std::cout << " took " << it_count << " iterations" << std::endl;
+
+    it_count = 0;
+    std::cout << std::fixed << std::setprecision(PRECISION) << regulaFalsi(2.7,5,it_count);
     std::cout << " took " << it_count << " iterations" << std::endl;
 }
